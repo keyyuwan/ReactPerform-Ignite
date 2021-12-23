d56e61d6d008accb2e514e900a9eac3ed86858b9
Key Yu Wan <keyflcbyuwan@gmail.com>
2021-12-22 22:39:52 -0300

    useMemo useCallback

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,23 @@
-import { FormEvent, useState } from "react";
+import { FormEvent, useState, useCallback } from "react";
 import { ProductsList } from "./components/ProductsList";
+
+interface Products {
+  totalPrice: number;
+  data: Array<{
+    id: number;
+    price: number;
+    priceFormatted: string;
+    title: string;
+  }>;
+}
 
 function App() {
   const [search, setSearch] = useState("");
 
-  const [products, setProducts] = useState([]);
+  const [products, setProducts] = useState<Products>({
+    totalPrice: 0,
+    data: [],
+  });
 
   async function handleSubmit(e: FormEvent) {
     e.preventDefault();
@@ -16,8 +29,30 @@ function App() {
     const res = await fetch(`http://localhost:3333/products?q=${search}`);
     const data = await res.json();
 
-    setProducts(data);
+    const formatted = new Intl.NumberFormat("pt-BR", {
+      style: "currency",
+      currency: "BRL",
+    });
+
+    const products = data.map((product: any) => {
+      return {
+        id: product.id,
+        title: product.title,
+        price: product.price,
+        priceFormatted: formatted.format(product.price),
+      };
+    });
+
+    const totalPrice = data.reduce((acc: number, product: any) => {
+      return acc + product.price;
+    }, 0);
+
+    setProducts({ totalPrice, data: products });
   }
+
+  const addToWishList = useCallback((id: number) => {
+    console.log(id);
+  }, []);
 
   return (
     <div>
@@ -32,7 +67,11 @@ function App() {
         <button type="submit">Search</button>
       </form>
 
-      <ProductsList products={products} />
+      <ProductsList
+        totalPrice={products.totalPrice}
+        products={products.data}
+        onAddToWishList={addToWishList}
+      />
     </div>
   );
 }
--- src/components/ProductsList.tsx
+++ src/components/ProductsList.tsx
@@ -1,18 +1,31 @@
 import { ProductItem } from "./ProductItem";
 
 interface ProductsListProps {
+  totalPrice: number;
   products: Array<{
     id: number;
     price: number;
+    priceFormatted: string;
     title: string;
   }>;
+  onAddToWishList: (id: number) => void;
 }
 
-export function ProductsList({ products }: ProductsListProps) {
+export function ProductsList({
+  totalPrice,
+  products,
+  onAddToWishList,
+}: ProductsListProps) {
   return (
     <div>
+      <h2>{totalPrice}</h2>
+
       {products.map((product) => (
-        <ProductItem product={product} />
+        <ProductItem
+          key={product.id}
+          product={product}
+          onAddToWishList={onAddToWishList}
+        />
       ))}
     </div>
   );

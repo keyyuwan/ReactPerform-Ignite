80079149973668629546e2653e1364fc5fc743ef
Key Yu Wan <keyflcbyuwan@gmail.com>
2021-12-23 22:02:48 -0300

    virtualization

--- src/components/ProductsList.tsx
+++ src/components/ProductsList.tsx
@@ -1,3 +1,4 @@
+import { List, ListRowRenderer } from "react-virtualized";
 import { ProductItem } from "./ProductItem";
 
 interface ProductsListProps {
@@ -16,17 +17,29 @@ export function ProductsList({
   products,
   onAddToWishList,
 }: ProductsListProps) {
+  const rowRenderer: ListRowRenderer = ({ index, key, style }) => {
+    return (
+      <div key={key} style={style}>
+        <ProductItem
+          product={products[index]}
+          onAddToWishList={onAddToWishList}
+        />
+      </div>
+    );
+  };
+
   return (
     <div>
       <h2>{totalPrice}</h2>
 
-      {products.map((product) => (
-        <ProductItem
-          key={product.id}
-          product={product}
-          onAddToWishList={onAddToWishList}
-        />
-      ))}
+      <List
+        height={300}
+        rowHeight={30}
+        width={900}
+        overscanRowCount={5} // quantos itens pra cima e pra baixo eu quero que ele pré carregue
+        rowCount={products.length} // quantidade total da lista
+        rowRenderer={rowRenderer} // função que vai renderizar os elementos
+      />
     </div>
   );
 }
